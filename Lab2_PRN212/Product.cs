6d7112c1afe5f605288f67fe6f96039920162024
Le Tan Dai <letandai1304@gmail.com>
2024-09-20 22:03:09 +0700

    lab2_prn212

---
+++ Lab2_PRN212/Product.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Product
+{
+    private string Name;
+    private double Price;
+    private double Discount;
+
+    public Product() { }
+    public Product(string name, double price, double discount)
+    {
+        Name = name;
+        Price = price;
+        Discount = discount;
+    }
+    public Product(string name,double price)
+    {
+        Name = name; 
+        Price = price;
+        Discount = 0.0;
+    }
+    public string name
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+    public double price
+    {
+        get { return Price; }
+        set
+        {
+            if(value >= 0)
+            {
+                Price = value;
+            }
+            else
+            {
+                throw new ArgumentException("Price can't be negative.");
+            }
+        }
+    }
+    public double discount
+    {
+        get { return Discount; }
+        set
+        {
+            if(value >= 0 && value <= 100)
+            {
+                Discount = value;
+            }
+            else
+            {
+                throw new ArgumentException("Discount must be between 0 and 100.");
+            }
+        }
+    }
+    private double GetImportTax()
+    {
+        double result = 0.1 * Price;
+        return result;
+    }
+    public void Input()
+    {
+        Console.Write("Enter product name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter product price: ");
+        Price = double.Parse(Console.ReadLine());
+        Console.Write("Enter discount: ");
+        Discount = double.Parse(Console.ReadLine());
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Product Name: {Name}");
+        Console.WriteLine($"Price: {Price}");
+        Console.WriteLine($"Discount: {Discount}%");
+    }
+}
---
+++ Lab2_PRN212/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        //Product product = new Product();
+        //product.Input();
+        //product.Display();
+        //Console.WriteLine($"Tax:{product.GetImportTax()}");
+        //Product pd1 = new Product();
+        //Product pd2 = new Product();
+        //pd1.Input();
+        //pd2.Input();
+        //pd2.Display();
+        //pd2.Display();
+        Product pd1 = new Product("Tra Sua",20000,10);
+        Product pd2 = new Product("Tra Dao", 15000);
+        Console.WriteLine("\nProduct 1 details information:");
+        pd1.Display();
+        Console.WriteLine("\nProduct 2 details information:");
+        pd2.Display();
+
+        pd1.name = "Tra Vai";
+        Console.WriteLine("\nUpdate product 1:");
+        pd1.Display();
+
+        pd2.price = 10000;
+        Console.WriteLine("\nUpdate product 2:");
+        pd2.Display();
+    }
+}
